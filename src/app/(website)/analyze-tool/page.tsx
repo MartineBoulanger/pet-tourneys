3102aae013a31f1023bb26133bb6dc5bdae4156b
MartineBoulanger <martine.boulanger@vaimo.com>
2025-04-26 01:18:29 +0200

    added metadata to the analyze tool page

--- src/app/(website)/analyze-tool/page.tsx
+++ src/app/(website)/analyze-tool/page.tsx
@@ -1,10 +1,21 @@
 import { Heading, Paragraph } from '@/components/ui';
 import { AnalyzeToolForm } from '@/components/analyze-tool/AnalyzeToolForm';
 
+export async function generateMetadata() {
+  return {
+    title: 'Analyze Tool',
+    alternates: {
+      canonical: `${process.env.NEXT_PUBLIC_BASE_URL!}/analyze-tool`,
+    },
+  };
+}
+
 export default function AnalyzerPage() {
   return (
     <div className='container mx-auto py-8'>
-      <Heading className='text-center'>{'Pet Battle PvP Logs Analyzer'}</Heading>
+      <Heading className='text-center'>
+        {'Pet Battle PvP Logs Analyzer'}
+      </Heading>
       <Paragraph className='text-center mb-2.5 max-w-[600px] mx-auto'>
         {'Analyze your personal pet battle PvP logs.'}
       </Paragraph>
