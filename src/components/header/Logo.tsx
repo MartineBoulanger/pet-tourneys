e1617c2ae575fcb594d43e68d96929960724b59b
MartineBoulanger <martine.boulanger@vaimo.com>
2025-07-05 23:37:48 +0200

    fixed size on logo in header

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 export const Logo = () => {
   return (
     <Link title='Logo' aria-label='Logo' href='/'>
-      <span className='w-10 h-10'>
+      <span>
         <img
           width={40}
           height={40}
-          className='w-full h-full object-contain'
+          className='w-12 h-12 object-contain'
           src={`${process.env.NEXT_PUBLIC_BASE_URL!}/images/tourney-logo.png`}
           alt='Logo'
           loading='lazy'
