acd7a82f7653d9203689f8039cc46b2c44e81f1d
MartineBoulanger <martine.boulanger@vaimo.com>
2025-08-24 01:51:28 +0200

    fixing mongoDB connection

--- src/mongoDB/client.ts
+++ src/mongoDB/client.ts
@@ -1,22 +1,27 @@
 import mongoose from 'mongoose';
 
-if (!global.mongoose) global.mongoose = { conn: null, promise: null };
+const MONGO_URI = process.env.MONGODB_URI!;
+if (!MONGO_URI)
+  throw new Error('Please define the MONGODB_URI environment variable');
+
+let cached = global.mongoose;
+if (!cached) cached = global.mongoose = { conn: null, promise: null };
 
 async function dbConnect() {
-  const uri = process.env.MONGODB_URI!;
-  if (!uri) throw new Error('Please add your Mongo URI to .env');
-  if (global.mongoose.conn) return global.mongoose.conn;
-  if (!global.mongoose.promise) {
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
     const opts = { bufferCommands: false };
-    global.mongoose.promise = mongoose.connect(uri, opts);
+    cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
   }
   try {
-    global.mongoose.conn = await global.mongoose.promise;
-    return global.mongoose.conn;
+    cached.conn = await cached.promise;
   } catch (error) {
-    global.mongoose.promise = null;
+    cached.promise = null;
     throw error;
   }
+  return cached.conn;
 }
 
 export default dbConnect;
