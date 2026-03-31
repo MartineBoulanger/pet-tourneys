b3538fd16ea9449916f41af846aa14772a7568c3
MartineBoulanger <martine.boulanger@gmail.com>
2026-03-31 09:45:43 +0200

    made a few little changes

--- src/lib/supabase/static.ts
+++ src/lib/supabase/static.ts
@@ -2,21 +2,20 @@
 
 import { createServerClient } from '@supabase/ssr';
 
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_ANON_KEY!;
+
 // ==================================================
 // server client with types from Supabase
 // - used mainly for the pets and cms schema tables
 // ==================================================
 export async function sbStatic<T>() {
-  return createServerClient<T>(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll: () => [],
-        setAll: () => {},
-      },
+  return createServerClient<T>(supabaseUrl, supabaseKey, {
+    cookies: {
+      getAll: () => [],
+      setAll: () => {},
     },
-  );
+  });
 }
 
 // ==================================================
@@ -24,14 +23,10 @@ export async function sbStatic<T>() {
 // - used mainly for the api schema tables
 // ==================================================
 export async function sbApiStatic() {
-  return createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll: () => [],
-        setAll: () => {},
-      },
+  return createServerClient(supabaseUrl, supabaseKey, {
+    cookies: {
+      getAll: () => [],
+      setAll: () => {},
     },
-  );
+  });
 }
