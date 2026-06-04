77f70ac844708b5a3368648cc493274fd5758647
MartineBoulanger <martine.boulanger@gmail.com>
2026-06-05 00:20:13 +0200

    added back button on pet details page

--- src/components/pets/PetDetails.tsx
+++ src/components/pets/PetDetails.tsx
@@ -14,6 +14,7 @@ import { PetAbilities } from './PetAbilities';
 import { PetDescription } from './PetDescription';
 import { getAbilitiesByNames } from '@/actions/supabase/pets-schema/abilities/getAbilities';
 import { getPetType } from '@/actions/supabase/pets-schema/families/getFamilies';
+import Link from 'next/link';
 
 export async function PetDetails({ pet }: { pet: Pet }) {
   const petTypeColor =
@@ -72,40 +73,52 @@ export async function PetDetails({ pet }: { pet: Pet }) {
   const family = familyData as Family;
 
   return (
-    <div className='bg-background p-2.5 lg:p-5 rounded-lg relative'>
-      <div className='grid md:grid-cols-2 gap-2.5 lg:gap-5'>
-        {/* Image */}
-        <PetImage pet={pet} />
-        {/* Details */}
-        <div className='space-y-2.5 lg:space-y-5 mt-2.5'>
-          <PetInformation
-            pet={pet}
-            petTypeColor={petTypeColor}
-            family={family}
-          />
-          <div className='rounded-full w-full h-[2px] my-5 bg-medium-grey' />
-          <PetBaseStats pet={pet} petTypeColor={petTypeColor} />
-          <div className='rounded-full w-full h-[2px] my-5 bg-medium-grey' />
-          <PetBreeds pet={pet} petTypeColor={petTypeColor} />
-          <div className='md:hidden rounded-full w-full h-[2px] my-5 mx-auto bg-medium-grey' />
-          {(pet.is_horde || pet.is_alliance) && (
-            <div className='absolute top-2 lg:top-5 right-2 lg:right-5'>
-              {pet.is_horde && <Horde className='w-20 h-20' />}
-              {pet.is_alliance && <Alliance className='w-20 h-20' />}
-            </div>
-          )}
+    <>
+      <div className='bg-background p-2.5 lg:p-5 rounded-lg relative'>
+        <div className='grid md:grid-cols-2 gap-2.5 lg:gap-5'>
+          {/* Image */}
+          <PetImage pet={pet} />
+          {/* Details */}
+          <div className='space-y-2.5 lg:space-y-5 mt-2.5'>
+            <PetInformation
+              pet={pet}
+              petTypeColor={petTypeColor}
+              family={family}
+            />
+            <div className='rounded-full w-full h-[2px] my-5 bg-medium-grey' />
+            <PetBaseStats pet={pet} petTypeColor={petTypeColor} />
+            <div className='rounded-full w-full h-[2px] my-5 bg-medium-grey' />
+            <PetBreeds pet={pet} petTypeColor={petTypeColor} />
+            <div className='md:hidden rounded-full w-full h-[2px] my-5 mx-auto bg-medium-grey' />
+            {(pet.is_horde || pet.is_alliance) && (
+              <div className='absolute top-2 lg:top-5 right-2 lg:right-5'>
+                {pet.is_horde && <Horde className='w-20 h-20' />}
+                {pet.is_alliance && <Alliance className='w-20 h-20' />}
+              </div>
+            )}
+          </div>
         </div>
+        {/* Description */}
+        <PetDescription pet={pet} petTypeColor={petTypeColor} />
+        {/* Abilities */}
+        <PetAbilities
+          pet={pet}
+          petTypeColor={petTypeColor}
+          abilities={abilities}
+        />
+        {/* Pet Type Image on background */}
+        <PetTypeImage pet={pet} />
       </div>
-      {/* Description */}
-      <PetDescription pet={pet} petTypeColor={petTypeColor} />
-      {/* Abilities */}
-      <PetAbilities
-        pet={pet}
-        petTypeColor={petTypeColor}
-        abilities={abilities}
-      />
-      {/* Pet Type Image on background */}
-      <PetTypeImage pet={pet} />
-    </div>
+      <div className='flex justify-end mt-5'>
+        <Link
+          href='/pets'
+          className='btn-submit flex items-center gap-2.5 py-[7px] px-[11px] rounded-lg'
+          title={'Back To Overview'}
+          aria-label={'Back To Overview'}
+        >
+          {'Back To Overview'}
+        </Link>
+      </div>
+    </>
   );
 }
